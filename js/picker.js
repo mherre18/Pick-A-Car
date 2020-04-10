344334799f2ee86b80cc0dbd8112583331881a22
mherre18 <manulilla83@gmail.com>
2020-04-10 17:12:48 -0500

    year selection

--- js/picker.js
+++ js/picker.js
@@ -2,7 +2,7 @@ const years = document.createElement('option');
 const max = new Date().getFullYear();
 let min = max -10;
 
-for (let i = max; i > min; 1--){
+for (let i = max; i > min; i--){
     let option =  document.createElement('option');
     option.value = i;
     option.innerText = i;
